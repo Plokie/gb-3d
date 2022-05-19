5e330ee7c7f91d0f95d32cc252260a4b7ee37a68
Plokie <larazadexd@gmail.com>
2022-05-19 13:22:07 +0100

    Linux building

--- compile.bat
+++ compile.bat
@@ -1,3 +1,4 @@
 REM Automatically generated from Makefile
 ..\..\gbdk-win\gbdk\bin\lcc -o test_game.gb src\main.c  res\tiles.c
 REM res\GBcraftTiles.c res\GBcraftSprites.c
+REM For linux: make test_game.gb src/main.c res/tiles.c
---
+++ make.sh
@@ -0,0 +1,1 @@
+make test_game.gb src/main.c res/tiles.c
